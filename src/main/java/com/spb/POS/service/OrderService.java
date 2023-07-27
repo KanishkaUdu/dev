159c5ee97c357feb7373e2a1bf1c2b9981fd106a
KANISHKAU <kanishkau@mobitel.lk>
2023-07-27 11:57:38 +0530

    first commit

---
+++ src/main/java/com/spb/POS/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.spb.POS.service;
+
+import com.spb.POS.dto.request.RequestOrderSaveDTO;
+
+public interface OrderService {
+    String addOrder(RequestOrderSaveDTO requestOrderSaveDTO);
+}
